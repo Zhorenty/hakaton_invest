2130cb3e79ec514ee79c7046ec9d7ca0383e9c62
George <zhorenty@gmail.com>
2023-02-19 13:28:05 +0300

    Add mPropery models

--- mobile_app/lib/view/home/widgets/property_card_widget.dart
+++ mobile_app/lib/view/home/widgets/property_card_widget.dart
@@ -15,129 +15,162 @@ class PropertyCard extends StatelessWidget {
   @override
   //обернуть в CustomScrollView or SliverList
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Stack(
-          children: [
-            Center(
-              child: ClipRRect(
-                borderRadius: BorderRadius.circular(20),
-                child: Image.network(
-                  // '${property.imageUrl}'
-                  'https://mykaleidoscope.ru/uploads/posts/2022-08/1660216443_22-mykaleidoscope-ru-p-krasivie-chastnii-dom-dizain-krasivo-foto-22.jpg',
-                  fit: BoxFit.fill,
-                ),
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.only(left: 13, top: 10),
-              child: Row(
-                children: const [
-                  SizedBox(
-                    height: 26,
-                    width: 77,
-                    child: Chip(
-                      // label: Text(property!.isRisked.toString()),
-                      label: Align(
-                        // label: Text(property!.isCommercial.toString()),
-                        alignment: Alignment.topCenter,
-                        child: Text(
-                          'Риск',
-                          style: TextStyle(color: Colors.white),
+    Property home1 = Property(
+      id: 0,
+      description: '',
+      location: 'Дальняя улица, 8к1, Краснодар, \nКраснодарский край',
+      imageUrl:
+          // 'https://mykaleidoscope.ru/uploads/posts/2022-08/1660216443_22-mykaleidoscope-ru-p-krasivie-chastnii-dom-dizain-krasivo-foto-22.jpg',
+          'https://cdn-p.cian.site/images/06/446/341/kottedzh-sochi-voroshilovgradskaya-ulica-1436446026-1.jpg',
+      price: 14000000,
+      potentialPercentProfitPerYear: 7,
+      isRisked: false,
+      isCommercial: false,
+    );
+    Property home2 = Property(
+      id: 1,
+      description: '',
+      location: 'Краснодарский край, Сочи, р-н Адлерский',
+      imageUrl:
+          'https://mykaleidoscope.ru/uploads/posts/2022-08/1660216443_22-mykaleidoscope-ru-p-krasivie-chastnii-dom-dizain-krasivo-foto-22.jpg',
+      price: 12750000,
+      potentialPercentProfitPerYear: 5,
+      isRisked: true,
+      isCommercial: false,
+    );
+
+    List<Property> examples = [home1, home2];
+    return SizedBox(
+      height: 1000,
+      width: 500,
+      child: ListView.builder(
+          physics: const NeverScrollableScrollPhysics(),
+          itemCount: examples.length,
+          itemBuilder: (context, index) {
+            return Column(
+              children: [
+                Stack(
+                  children: [
+                    Center(
+                      child: ClipRRect(
+                        borderRadius: BorderRadius.circular(20),
+                        child: Image.network(
+                          '${examples[index].imageUrl}',
+                          // 'https://mykaleidoscope.ru/uploads/posts/2022-08/1660216443_22-mykaleidoscope-ru-p-krasivie-chastnii-dom-dizain-krasivo-foto-22.jpg',
+                          fit: BoxFit.fill,
                         ),
                       ),
-                      backgroundColor: Color(0xffFF5858),
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.all(Radius.circular(20)),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(left: 13, top: 10),
+                      child: Row(
+                        children: [
+                          SizedBox(
+                            height: 26,
+                            width: 77,
+                            child: Chip(
+                              label: Align(
+                                alignment: Alignment.topCenter,
+                                child: Text(
+                                  examples[index].isRisked.toString(),
+                                  style: const TextStyle(color: Colors.white),
+                                ),
+                              ),
+                              backgroundColor: const Color(0xffFF5858),
+                              shape: const RoundedRectangleBorder(
+                                borderRadius:
+                                    BorderRadius.all(Radius.circular(20)),
+                              ),
+                            ),
+                          ),
+                          const SizedBox(width: 5),
+                          SizedBox(
+                            height: 25,
+                            width: 80,
+                            child: Chip(
+                              // label: Text(),
+                              label: Align(
+                                alignment: Alignment.topCenter,
+                                child: Text(
+                                  examples[index].isCommercial.toString(),
+                                  style: const TextStyle(color: Colors.white),
+                                ),
+                              ),
+                              backgroundColor:
+                                  const Color.fromARGB(255, 24, 45, 235),
+                              shape: const RoundedRectangleBorder(
+                                borderRadius:
+                                    BorderRadius.all(Radius.circular(20)),
+                              ),
+                            ),
+                          ),
+                        ],
                       ),
                     ),
-                  ),
-                  SizedBox(width: 5),
-                  SizedBox(
-                    height: 25,
-                    width: 80,
-                    child: Chip(
-                      // label: Text(property!.isCommercial.toString()),
-                      label: Align(
-                        alignment: Alignment.topCenter,
-                        child: Text(
-                          'Жилая',
-                          style: TextStyle(color: Colors.white),
+                  ],
+                ),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    const SizedBox(height: 5),
+                    Row(
+                      children: [
+                        examples[index].potentialPercentProfitPerYear.isNegative
+                            ? const Icon(
+                                Icons.arrow_upward,
+                                size: 25,
+                                color: Color.fromARGB(255, 42, 209, 89),
+                              )
+                            : const Icon(
+                                Icons.arrow_downward,
+                                size: 25,
+                                color: Color.fromARGB(255, 209, 42, 42),
+                              ),
+                        // SizedBox(
+                        //   width: 11,
+                        //   child: Icon(
+                        //     CupertinoIcons.arrow_up,
+                        //     size: 22,
+                        //     color: Color.fromARGB(255, 42, 209, 89),
+                        //   ),
+                        // ),
+
+                        const SizedBox(width: 6),
+                        Text(
+                          examples[index]
+                              .potentialPercentProfitPerYear
+                              .toString(),
+                          style: TextStyle(
+                              color: examples[index]
+                                      .potentialPercentProfitPerYear
+                                      .isNegative
+                                  ? Colors.red
+                                  : Colors.green,
+                              fontWeight: FontWeight.bold),
                         ),
-                      ),
-                      backgroundColor: Color.fromARGB(255, 24, 45, 235),
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.all(Radius.circular(20)),
+                      ],
+                    ),
+                    const SizedBox(height: 5),
+                    Text(
+                      '${examples[index].price.toInt()} ₽',
+                      // '12 750 000 ₽',
+                      style: const TextStyle(
+                        // fontWeight: FontWeight.bold,
+                        fontFamily: 'Outfit',
+                        fontSize: 25,
                       ),
                     ),
-                  ),
-                ],
-              ),
-            ),
-          ],
-        ),
-        Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            const SizedBox(height: 5),
-            Row(
-              children: const [
-                // property.potentialPercentProfitPerYear.isNegative
-                //     ? Icon(
-                //         Icons.arrow_upward,
-                //         size: 25,
-                //         color: Color.fromARGB(255, 42, 209, 89),
-                //       )
-                //     : Icon(
-                //         Icons.arrow_downward,
-                //         size: 25,
-                //         color: Color.fromARGB(255, 209, 42, 42),
-                //       ),
-                SizedBox(
-                  width: 11,
-                  child: Icon(
-                    CupertinoIcons.arrow_up,
-                    size: 22,
-                    color: Color.fromARGB(255, 42, 209, 89),
-                  ),
+                    //ЗАМУТИ ПЕРЕНОС СТРОКИ ЭРИК
+                    Text(
+                      examples[index].location,
+                      style: const TextStyle(fontSize: 16),
+                    ),
+                  ],
                 ),
-                // Text(
-                //   property.potentialPercentProfitPerYear,
-                //   style: TextStyle(
-                //       color: property.potentialPercentProfitPerYear.isNegative
-                //           ? Colors.red
-                //           : Colors.green,
-                //       fontWeight: FontWeight.bold),
-                // ),
-                SizedBox(width: 6),
-                Text(
-                  '5% в год',
-                  style: TextStyle(
-                    color: Colors.green,
-                    fontSize: 18,
-                  ),
-                ),
+                const SizedBox(height: 25),
               ],
-            ),
-            const SizedBox(height: 5),
-            const Text(
-              // '${property.price}₽'
-              '12 750 000 ₽',
-              style: TextStyle(
-                // fontWeight: FontWeight.bold,
-                fontFamily: 'Outfit',
-                fontSize: 25,
-              ),
-            ),
-            //ЗАМУТИ ПЕРЕНОС СТРОКИ ЭРИК
-            const Text(
-              // '${property.location}'
-              'Дальняя улица, 8к1, Краснодар, \nКраснодарский край',
-              style: TextStyle(fontSize: 16),
-            ),
-          ],
-        ),
-      ],
+            );
+          }),
     );
   }
 }
